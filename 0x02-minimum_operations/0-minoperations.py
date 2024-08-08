df6ea9dcc881540bd3570b1db8aefe4c6722eb86
dawit <dawitxxd@gmail.com>
2024-08-08 21:51:38 +0300

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H. Your text editor can execute
+only two operations in this file: Copy All and Paste. Given a number n, write
+a method that calculates the fewest number of operations needed to result in
+exactly n H characters in the file.
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+    Calculates the fewest number of operations needed to result
+    in exactly n 'H' characters in the file.
+
+    Args:
+      n: The desired number of 'H' characters.
+
+    Returns:
+      The minimum number of operations needed.
+    """
+
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 0  # Already have one 'H'
+
+    operations = 0
+    h_count = 1i
+    clipboard = 1
+
+    while h_count < n:
+        """If n is divisible by current h_count, pasting
+           is the most efficient way"""
+        if n % h_count == 0:
+            clipboard = h_count
+            operations += 1  # For Copy All
+
+        h_count += clipboard
+        operations += 1  # For Paste
+
+    return operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+minimum operations required to get n number of character H in a file, only copy All and pase operations permited.
