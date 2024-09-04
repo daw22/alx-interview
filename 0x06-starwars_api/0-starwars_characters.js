b258892e2bec303091d1c099974735ef34ac46c1
dawit <dawitxxd@gmail.com>
2024-09-04 22:33:03 +0300

    star wars api characters list

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,13 +6,16 @@ const movieId = process.argv[2];
 if (movieId) {
   const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
   let charactersUrls;
-  request(url, async function (error, response, body) {
+  request(url, function (error, response, body) {
     if (!error) {
       charactersUrls = JSON.parse(body).characters;
       for (let i = 0; i < charactersUrls.length; i++) {
-        const character = await fetch(charactersUrls[i]);
-        const charData = await character.json();
-        console.log(charData.name);
+        request(charactersUrls[i], function (error, response, body) {
+          if (!error) {
+            const name = JSON.parse(body).name;
+            console.log(name);
+          }
+        });
       }
     }
   });
