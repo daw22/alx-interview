280ccdf86ff787fee101fd36f319b9e976c6f032
dawit <dawitxxd@gmail.com>
2024-09-26 14:54:25 +0300

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Island perimeter problem
+"""
+
+
+def island_perimeter(grid):
+    """
+    solution for island perimeter problem
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            num = grid[i][j]
+            if num == 1:
+                nums = []
+                if i > 0:
+                    nums.append(grid[i - 1][j])
+                if j > 0:
+                    nums.append(grid[i][j - 1])
+                if i < len(grid[i]) - 1:
+                    nums.append(grid[i + 1][j])
+                if j < len(grid):
+                    nums.append(grid[i][j + 1])
+                perimeter += 4 - sum(nums)
+    return perimeter
